a2ab2fe4913f8c058ec415b94e7995d9e52552e0
asus <asus@Swapnil05>
2024-02-23 16:31:29 -0500

    second commit

---
+++ src/midtermexam_versiona_extensioncode/swapnil_Design.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author asus
+ */
+public class swapnil_Design {
+    
+    //My favourite subject is Fundamentals of Software Design and Development
+    //I like this course because it is practical based and we learn a lot of on-hands stuff in the classes
+    //I always wondered what git was and how it all worked and now that I've learned it, it is really a great subject
+    //All in all, this subject is really intriguing for me and I'm glad it is a part of the curriculum
+    //Thank You!
+    
+}
